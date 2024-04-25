f79f5319754c208234c945a7bf2dc94cd73661bc
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2024-04-25 22:10:13 +0300

    moved to backend folder

--- backend/gymrat/schemas/workout.py
+++ backend/gymrat/schemas/workout.py
@@ -1,7 +1,7 @@
 from datetime import date
 from typing import Optional, List
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel
 
 from gymrat.schemas.exercise import ExerciseOut
 
